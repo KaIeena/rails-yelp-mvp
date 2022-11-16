bd0cd0012964a2548063c289f1f4a7d3f666c813
Angela Garcia <angela.garcia.pro@outlook.com>
2022-11-16 12:49:53 +0100

    rails yelp mvp

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+
+  validates :name, :address, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destroy_all
+
+Restaurant.create(name: "Supergirl", address: "National City", phone_number: "126", category: "chinese")
+
+Restaurant.create(name: "Flash", address: "Central City", phone_number: "171", category: "italian")
+
+Restaurant.create(name: "Arrow", address: "Star City", phone_number: "170", category: "french")
+
+Restaurant.create(name: "Wonder Woman", address: "Themyscira", phone_number: "130", category: "belgian")
+
+Restaurant.create(name: "Batman", address: "Gotham City", phone_number: "126", category: "japanese")
